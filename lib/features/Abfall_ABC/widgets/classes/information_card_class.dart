03ab5bdbcd15c26d6c76f8b0eb73953251bbd225
Dirk Kraft <Dirk.kraft.akademie@gmail.com>
2024-10-18 16:47:36 +0200

    Information Card Class created

---
+++ lib/features/Abfall_ABC/widgets/classes/information_card_class.dart
@@ -0,0 +1,12 @@
+class InformationCardClass {
+  final String title1;
+  final String trashDescription;
+  final String title2;
+  final String desposalDescription;
+
+  InformationCardClass(
+      {required this.title1,
+      required this.trashDescription,
+      required this.title2,
+      required this.desposalDescription});
+}
