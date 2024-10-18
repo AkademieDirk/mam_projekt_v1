9aafcb839c09ab278e9d8da5d78959986c550e0e
Dirk Kraft <Dirk.kraft.akademie@gmail.com>
2024-10-18 15:25:06 +0200

    Theme added to the project

---
+++ lib/config/themes/themes.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:mam_projekt_v1/config/sizes/sizes.dart';
+
+ThemeData mamTheme = ThemeData(
+    fontFamily: "Urbanist",
+    textTheme: const TextTheme(
+        bodyMedium: TextStyle(fontSize: normaTextSize),
+        headlineMedium: TextStyle(fontSize: bigTextSize),
+        bodySmall: TextStyle(fontSize: smallTextSize)));
