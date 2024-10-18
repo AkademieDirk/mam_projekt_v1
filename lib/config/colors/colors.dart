375d2db3228ba66859c2d21bafecf0b8c10d11c8
Dirk Kraft <Dirk.kraft.akademie@gmail.com>
2024-10-18 15:45:46 +0200

    Colors edited

--- lib/config/colors/colors.dart
+++ lib/config/colors/colors.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
 const Color greenText = Color.fromARGB(255, 23, 217, 210);
-const Color backgroundGradientStartColor = Color.fromARGB(255, 52, 62, 62);
+const Color backgroundGradientStartColor = Color.fromARGB(255, 153, 170, 170);
 const Color backgroundGradientEndColor = Color.fromARGB(255, 81, 202, 202);
 const Color buttonGradientStartColor = Color.fromARGB(255, 107, 117, 117);
 const Color buttonGradientEndColor = Color.fromARGB(255, 136, 177, 177);
 const Color objectsdarkColor = Color.fromARGB(255, 5, 42, 16);
-const Color objectslightColor = Color.fromARGB(255, 22, 90, 28);
+const Color objectslightColor = Color.fromARGB(255, 107, 117, 108);
