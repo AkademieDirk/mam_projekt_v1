72d7d66004e085e31e8fead83c78e33bced757dd
Dirk Kraft <Dirk.kraft.akademie@gmail.com>
2024-10-18 15:27:54 +0200

    Main Test

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:mam_projekt_v1/config/colors/colors.dart';
+import 'package:mam_projekt_v1/config/themes/themes.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,10 +11,23 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      theme: mamTheme,
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        body: Container(
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [
+                backgroundStartColor,
+                backgroundEndColor,
+              ])),
+          child: const Center(
+            child: Text(
+              'Hello World!',
+            ),
+          ),
         ),
       ),
     );
